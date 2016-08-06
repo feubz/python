affed27b693553552aba4c966a01b63670d4c355
Adriano BANFI <adrianobanfi@Mac-mini.fibrestream.ca>
2016-08-06 18:55:41 -0400

    number guessing update

--- dice-rolling-simulator.py
+++ dice-rolling-simulator.py
@@ -1,6 +1,11 @@
 from random import randint
+number = randint(1,6)
+choice = 0
 while True:
-	print(randint(1,6))
-	choice = raw_input("Do you want to continue? : ")
-	if choice not in ('yes','y','YES'):
+	try:
+		choice = int(raw_input("What is your guess? : "))
+	except ValueError:
+			print("Oopps, this is not a number")
+	if choice == number:
+		print ("You won")
 		break
