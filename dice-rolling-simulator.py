627553331c694d52d82f234980db9c568a851728
Adriano BANFI <adrianobanfi@Mac-mini.fibrestream.ca>
2016-08-06 18:14:34 -0400

    change response model

--- dice-rolling-simulator.py
+++ dice-rolling-simulator.py
@@ -2,5 +2,5 @@ from random import randint
 while True:
 	print(randint(1,6))
 	choice = raw_input("Do you want to continue? : ")
-	if choice not in ('yes'):
+	if choice not in ('yes','y','YES'):
 		break
