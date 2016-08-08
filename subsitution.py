ff08550933fce9e0e5d70dc90a1ac1df2b814508
Adriano BANFI <adrianobanfi@Mac-mini.fibrestream.ca>
2016-08-08 01:27:27 -0400

    Mmmmm Better

--- subsitution.py
+++ subsitution.py
@@ -5,6 +5,7 @@ import getpass
 
 whoami = getpass.getuser()
 script, infile, outfile = argv
+
 print('Hello {}, we are going to copy data from {} to {}'.format(whoami,infile,outfile))
 
 #open the file and read the content
@@ -13,19 +14,20 @@ in_data = open(infile).read()
 #verify that the output file exist
 print('Does the output file {} exist? {}'.format(outfile,exists(outfile)))
 
+#print in_data
+print('Where are going to write the following string into the file: {}'.format(in_data))
 
 #open the output file with write right and truncate
-out_file = open(outfile, 'r+')
-#write the data into the output file
-out_file.write(in_data)
+with open(outfile, 'r+') as out_file:
+	out_file.truncate()
+	out_file.write(in_data)
+	out_file.seek(0)
+	print(out_file.read())
 
-#Display the file content
-print(out_file.read())
 
-#print('The file contain the following line : \n {}').format(output)
 
-#display the output file
-out_file.close()
+
+
 
 
 
